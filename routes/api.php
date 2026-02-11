784fc0c4795b9aac1c409e36a3c3e7a4e5456569
NextZac <pzsebastian@gmail.com>
2026-02-11 14:23:37 +0200

    Added Internal readings endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Http\Request;
+use App\Models\EnergyReading;
 
 
 
@@ -18,6 +19,9 @@ Route::prefix('api')->group(function () {
         return response()->json(["status" => "ok", "db" => "ok"]);
     });
     Route::get('/readings', function (Request $req) {
-        dd($req->all());
+        if(!array_key_exists("location", $req->all())) {
+            return response()->json(["status" => "fail", "message" => "Location is a mandatory field."]);
+        }
+        return response()->json(["status" => "ok", "entries" => EnergyReading::whereBetween('created_at', [array_key_exists("start", $req->all()) ? $req->all()["start"] : "2000-01-01T00:00:00Z", array_key_exists("end", $req->all()) ? $req->all()["end"] : "2999-01-01T00:00:00Z"])->get()]);
     });
 });
