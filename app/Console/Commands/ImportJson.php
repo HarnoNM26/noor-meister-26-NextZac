1a7fdeb980f2aff92424b2179e80240fb0fa5e10
NextZac <pzsebastian@gmail.com>
2026-02-11 14:50:38 +0200

    Made duplication check functional

--- app/Console/Commands/ImportJson.php
+++ app/Console/Commands/ImportJson.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Validator;
 use App\Models\EnergyReading;
+use Carbon\Carbon;
 
 class ImportJson extends Command
 {
@@ -32,7 +33,7 @@ class ImportJson extends Command
             // Invalid Timestamp == Skip Record
             // Missing Location == EE
             // price_eur_mwh - If numeric, accept(can be negative), if string == skip
-            //duplicates == ignore // Duplicate is when a record with the same timestamp and location is available.
+            // Duplicates == ignore // Duplicate is when a record with the same timestamp and location is available.
             $msg = null;
             $entry_array = (array) $entry;
             if(!array_key_exists("timestamp", $entry_array) || !array_key_exists("price_eur_mwh", $entry_array)){
@@ -44,14 +45,15 @@ class ImportJson extends Command
             }
             $validator = Validator::make($entry_array, ["timestamp" => "date|date_format:Y-m-d\TH:i:sp"]);
 
-            if(!preg_match('/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/', $entry_array["timestamp"]) > 0)
+            if($validator->fails())
             {   
                 $msg = "Invalid timestamp, skipping.";
+                continue;
             }
             if(!is_numeric($entry_array["price_eur_mwh"])) {
                 $msg = "Invalid price_eur_mwh, skipping.";
             }
-            if(EnergyReading::where('created_at', $entry_array["timestamp"])->count() > 0 && EnergyReading::where('location', $entry_array["location"])->count() > 0){
+            if(EnergyReading::where('created_at', new Carbon($entry_array["timestamp"]))->count() > 0 && EnergyReading::where('location', $entry_array["location"])->count() > 0){
                 $msg = "Duplicate Found, skipping.";
             }
             if($msg != null) {
